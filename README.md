e3976ced7179d06521f77a35c2509a22b0416bb3
Sebastian Zieba <sebastian.zieba@cfa.harvard.edu>
2025-11-20 09:24:55 +0100

    add installation info

--- README.md
+++ README.md
@@ -3,3 +3,16 @@ Fear Of Missing mnemOnics
 
 This code will collect the JWST Calibrated Engineering Data also called mnemOnics in order to correct your light curves :) 
 https://mast.stsci.edu/portal/Mashup/Clients/jwstedb/jwstedb.html
+
+
+## Installation
+
+(basically just that you have the dependancies)
+create a new directory and git clone the github there:
+
+git clone https://github.com/sebastian-zieba/fomo.git
+
+conda create -n fomo python==3.13.0
+conda activate fomo
+cd Desktop/Projects/Open_Source/fomo/fomo
+pip install -e .
--- src/fomo/plot_mnemonics.py
+++ src/fomo/plot_mnemonics.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
 import spelunker
 
-spk2 = spelunker.load('/Users/ddeal/JWST-Treasure-Chest/', pid=1534)
+
+spk2 = spelunker.load('/Users/sebastianzieba/Desktop/Projects/Open_Source/fomo/tmp', pid=7675)
 spk2.mast_api_token = 'enter_mast_token_id_here' # input mast_api token here!
 
 fig, ax = plt.subplots(figsize=(12,4),dpi=200)
 
 ax = spk2.mnemonics_local('GUIDESTAR') # plots when the JWST tracks onto a new guidestars as a vertical line
-ax = spk2.mnemonics('SA_ZHGAUPST', 60067.84, 60067.9) # plots the start and end of high gain antenna movement
+ax = spk2.mnemonics('SA_ZHGAUPST') # plots the start and end of high gain antenna movement
 
 ax.plot(spk2.fg_time, spk2.fg_flux)
 plt.legend(loc=3)
